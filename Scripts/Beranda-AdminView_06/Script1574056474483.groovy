720f13474484da5a8b986d73894c018a1fb95bc8
sehatiqa <sehatiqa@gmail.com>
2019-11-18 12:56:07 +0700

    add TC Beranda-AdminView_06

---
+++ Scripts/Beranda-AdminView_06/Script1574056474483.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.webui.keyword.internal.WebUIAbstractKeyword
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.apache.commons.lang.RandomStringUtils as RandomStringUtils
+
+WebUI.openBrowser('https://cc-staging.sehati.co/admin')
+
+WebUI.maximizeWindow()
+
+WebUI.delay(2)
+
+WebUI.setText(findTestObject('Page_Consultation Center/Login_Screen/field_email'), 'rudiadmin@mailinator.com')
+
+WebUI.delay(2)
+
+WebUI.setText(findTestObject('Page_Consultation Center/Login_Screen/field_password'), 'qwerty')
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('Page_Consultation Center/Login_Screen/button_Masuk'))
+
+WebUI.delay(3)
+
+WebUI.verifyElementPresent(findTestObject('Page_Consultation Center/Beranda (Admin)/button_buatdokter'), 0)
+
+WebUI.setText(findTestObject('Object Repository/Page_Consultation Center/Beranda (Admin)/field_caridokter'), 'Qa Kk')
+
+WebUI.delay(3)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Page_Consultation Center/Beranda (Admin)/namadokter1'), 'Qa Kk')
+
+WebUI.closeBrowser()
