f5181643e34779ae2ea375e6ac8354bc55473ea1
sehatiqa <sehatiqa@gmail.com>
2019-12-05 20:36:16 +0700

    global variable commit

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -9,6 +9,21 @@ import com.kms.katalon.core.main.TestCaseMain
  */
 public class GlobalVariable {
      
+    /**
+     * <p></p>
+     */
+    public static Object dokterid
+     
+    /**
+     * <p></p>
+     */
+    public static Object password
+     
+    /**
+     * <p></p>
+     */
+    public static Object adminid
+     
 
     static {
         try {
@@ -16,6 +31,9 @@ public class GlobalVariable {
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
             selectedVariables += RunConfiguration.getOverridingParameters()
     
+            dokterid = selectedVariables['dokterid']
+            password = selectedVariables['password']
+            adminid = selectedVariables['adminid']
             
         } catch (Exception e) {
             TestCaseMain.logGlobalVariableError(e)
