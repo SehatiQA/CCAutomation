5b25a919994305bc7b692f60e5cec4cc3f27f0d5
sehatiqa <sehatiqa@gmail.com>
2019-12-05 20:41:25 +0700

    add details pasien test suite

---
+++ Test Suites/Consultation Center - Details Pasien.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Consultation Center - Details Pasien</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c525b413-1917-4a3e-be9d-a73557a255c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>75dec434-4c08-4d66-a37b-62c583c5c590</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pasien_Details (Dokter View)_04</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
