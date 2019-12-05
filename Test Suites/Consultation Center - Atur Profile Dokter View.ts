e03978f2003b465fd1fcd9acaaf03b02764bf644
sehatiqa <sehatiqa@gmail.com>
2019-12-05 21:03:16 +0700

    add Atur Profil (Dokter View)_04, add atur profile dokterview testsuite

---
+++ Test Suites/Consultation Center - Atur Profile Dokter View.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Consultation Center - Atur Profile Dokter View</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>fa29cfa2-4d8c-468c-bb21-659ae95c99a7</testSuiteGuid>
+   <testCaseLink>
+      <guid>f419efa3-163f-406b-9f24-818a9c845cba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Atur Profil (Dokter View)_04</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
